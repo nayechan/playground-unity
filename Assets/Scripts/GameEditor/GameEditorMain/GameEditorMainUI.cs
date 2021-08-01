e150dfc88a7af6c17d0ea0d7a8dced9bb2d3e7db
nayechan <66f@naver.com>
2021-08-01 21:59:50 +0900

    Temporary commit

--- Assets/Scripts/GameEditor/GameEditorMain/GameEditorMainUI.cs
+++ Assets/Scripts/GameEditor/GameEditorMain/GameEditorMainUI.cs
@@ -5,48 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class GameEditorMainUI : MonoBehaviour
 {
-    [SerializeField] private GameEditorMoveScene moveScene;
-    List<string> scenePathList;
-    private string sceneDir;
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-        scenePathList = new List<string>();
-
-        Scene currentScene = SceneManager.GetActiveScene();
-        int sceneCount = SceneManager.sceneCountInBuildSettings;
-
-        sceneDir = SceneUtility.GetScenePathByBuildIndex(
-            currentScene.buildIndex
-        );
-
-        sceneDir = sceneDir.Substring(
-            0, 
-            sceneDir.Length - System.IO.Path.GetFileName(sceneDir).Length
-        );
-
-        Debug.Log(sceneDir);
-
-        for(int i=0;i<sceneCount;++i)
-        {
-            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
-
-            string dir = scenePath.Substring(
-                0, 
-                scenePath.Length - System.IO.Path.GetFileName(scenePath).Length
-            );
-
-            if(System.IO.Path.GetFileNameWithoutExtension(scenePath)
-            == currentScene.name) continue;
-
-            if(dir != sceneDir) continue;
-            scenePathList.Add(scenePath);
-        }
-    }
-    public void SceneChange(string sceneName)
-    {
-        moveScene.MoveTo(sceneName);
-        if(!scenePathList.Contains(sceneDir + sceneName + ".unity"))
-            Destroy(gameObject);
-    }
+    
 }
--- Assets/Scripts/GameEditor/GameEditorMain/GameEditorSceneManager.cs
+++ Assets/Scripts/GameEditor/GameEditorMain/GameEditorSceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameEditorMoveScene : MonoBehaviour
+public class GameEditorSceneManager : MonoBehaviour
 {
     [SerializeField] GameEditorDataManager.GameEditorDataManager dataManager;
     [SerializeField] private string currentScene;
