38f1491d6b766e0460c53e0670659d8a356bf257
Yechan Na <nayechan@hansung.ac.kr>
2021-08-03 00:47:17 +0900

    WIP : Object editor bug fix + JSON Structuring

--- Assets/Scripts/GameEditor/GameEditorMain/GameEditorDataManager.cs
+++ Assets/Scripts/GameEditor/GameEditorMain/GameEditorDataManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,35 @@ using UnityEngine.UI;
 
 namespace GameEditorDataManager
 {
+    [Serializable]
+    public class SerializableDictionary<K, V>
+    {
+        public Dictionary<K,V> dictionary;
+        public SerializableDictionary()
+        {
+            dictionary = new Dictionary<K, V>();
+        }
+    }
     public class MapEditorData{
         public class TileType{
-            Sprite sprite;
+            string spritePath;
             string name;
         }
         public class TileData{
             Vector3 position;
             System.Guid tileGuid;
         }
-        Dictionary<System.Guid, TileType> tileTypes;
+        SerializableDictionary<System.Guid, TileType> tileTypes;
         List<TileData> tileDatas;
     }
     public class ObjectEditorData{
+        public class ObjectType{
+            private Sprite[] sprites;
+            private string objectName;
+            private string objectType;
+            private float width, height;
+            private System.Guid guid;
+        }
         public class ObjectData{
             Vector3 _position;
             System.Guid _objectGuid;
@@ -27,11 +44,11 @@ namespace GameEditorDataManager
                 _objectGuid = objectGuid;
             }
         }
-        Dictionary<System.Guid, ObjectPrimitiveData> objectTypes;
+        SerializableDictionary<System.Guid, ObjectPrimitiveData> objectTypes;
         List<ObjectData> objectDatas;
         public void AddObjectType(ObjectPrimitiveData data)
         {
-            objectTypes.Add(data.GetGuid(),data);
+            objectTypes.dictionary.Add(data.GetGuid(),data);
         }
         public void AddObjectData(ObjectData data)
         {
@@ -41,29 +58,65 @@ namespace GameEditorDataManager
     public class EventEditorData{
 
     }
+    public class JsonData{
+        MapEditorData mapEditorData;
+        ObjectEditorData objectEditorData;
+        public void setMapEditorData(MapEditorData data)
+        {
+            mapEditorData = data;
+        }
+        public void setObjectEditorData(ObjectEditorData data)
+        {
+            objectEditorData = data;
+        }
+    }
+
+    public class A{
+        [SerializeField] int a;
+        [SerializeField] B _b;
+
+        public A(int a, int b, int c, int d)
+        {
+            this.a = a;
+            _b = new B(b,c,d);
+        }
+
+    }
+
+    [Serializable]
+    public class B{
+        [SerializeField] int b;
+        [SerializeField] Dictionary<int,int> c;
+        public B(int b, int c, int d)
+        {
+            this.b = b;
+            this.c = new Dictionary<int, int>();
+            this.c.Add(c,d);
+            this.c.Add(d,c);
+        }
+    }
+
     public class GameEditorDataManager : MonoBehaviour
     {
-        MapEditorData _mapEditorData;
-        ObjectEditorData _objectEditorData;
-        public void SaveMapEditorData(MapEditorData mapEditorData)
-        {
-            _mapEditorData = mapEditorData;
+        JsonData jsonData = new JsonData();
+
+        private void Start() {
+            Debug.Log(JsonUtility.ToJson(new A(1,2,3,4)));
         }
-        public void SaveObjectEditorData(ObjectEditorData objectEditorData)
+
+        public void setMapEditorData(MapEditorData data)
         {
-            _objectEditorData = objectEditorData;
+           jsonData.setMapEditorData(data);
         }
-        public MapEditorData LoadMapEditorData()
+
+        public void setObjectEditorData(ObjectEditorData data)
         {
-            return _mapEditorData;
+            jsonData.setObjectEditorData(data);
         }
-        public ObjectEditorData LoadObjectEditorData()
-        {
-            return _objectEditorData;
-        }
+
         public void printJsonString()
         {
-            Debug.Log(JsonUtility.ToJson(this));
+            Debug.Log(JsonUtility.ToJson(jsonData));
         }
     }
 }
